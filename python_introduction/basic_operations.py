14d1c6c6d379ac5b3aa2dd3f03f2d5bf2daf7d80
epowei <erejef@yahoo.com>
2024-12-10 14:06:51 +0100

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# Define variables
+number1 = 10
+number2 = 5
+
+# Addition operation
+sum = number1 + number2
+
+# Subtraction operation
+difference = number1 - number2
+
+# Multiplication operation
+product = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
