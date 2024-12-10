8e35d7bd9f863ef27d792602e4955e31f920088f
epowei <erejef@yahoo.com>
2024-12-10 14:28:48 +0100

    second commit

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Python script to calculate simple interest
+
+principal = 1000 # Principal amount in USD
+rate = 0.05 # Annual interest rate
+time = 3 # Time in years
+
+# Calculate simple interest
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
+
